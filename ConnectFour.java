c5330f736dbb5c5b443217412acfc317137f3ed9
Zelfire <rhong612@gmail.com>
2016-03-21 19:50:13 -0700

    GUI Updates
    -Program now outputs winner on GUI when game finishes
    -Added GUI button to start new game
    -Added GUI button to allow user to change number of connections and grid
    size

--- ConnectFour.java
+++ ConnectFour.java
@@ -14,6 +14,16 @@ public class ConnectFour {
     JFrame frame = new JFrame();
     private JButton topButton;
     private boolean win;
+    private JPanel uiPanel;
+    private JFrame settingsWindow;
+    
+    //These values should be changed later. 3 and 12 were just random numbers I used for testing purposes.
+    private int ROW_MIN = 3;
+    private int ROW_MAX = 12;
+    private int COLUMN_MIN = 3;
+    private int COLUMN_MAX = 12;
+    private int CONNECTIONS_MIN = 3;
+    private int CONNECTIONS_MAX = 12;
     
     /**
      * Constructor.
@@ -91,7 +101,7 @@ public class ConnectFour {
                 break;
             }
         }
-        initComponents();
+        uiPanel.repaint();
         win = hasWin();
     }
     
@@ -116,8 +126,8 @@ public class ConnectFour {
                 	count++;
                 	if (count == requiredConections)
                 	{
-                		// Will need to output winner on GUI. Would be cool to outline connection elements
-                		System.out.println(plyrClr + "player wins");
+                		// Would be cool to outline connection elements
+                	    	JOptionPane.showMessageDialog(frame, plyrClr + "player wins", "Game finished", JOptionPane.PLAIN_MESSAGE);
                 		win = true;
                         break;
                 	} 
@@ -142,8 +152,8 @@ public class ConnectFour {
                 	count++;
                 	if (count == requiredConections)
                 	{
-                		// Will need to output winner on GUI. Would be cool to outline connection elements
-                		System.out.println(plyrClr + "player wins");
+                		// Would be cool to outline connection elements
+            			JOptionPane.showMessageDialog(frame, plyrClr + "player wins", "Game finished", JOptionPane.PLAIN_MESSAGE);
                 		win = true;
                         break;
                 	} 
@@ -164,10 +174,9 @@ public class ConnectFour {
     private void initComponents() {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
         final DrawGrid dg = new DrawGrid();
 
-        JPanel uiPanel = new JPanel() {
+        uiPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics grphcs) {
                 super.paintComponent(grphcs);
@@ -229,7 +238,7 @@ public class ConnectFour {
             public void mousePressed(MouseEvent e) {
 
                 redraw(e.getX()); 
-                
+        	
                 if (currentPlayer == 1){
                     topButton.setBackground(Color.YELLOW); 
                 }
@@ -238,10 +247,124 @@ public class ConnectFour {
                 }
             }
         });
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Game");
+        menuBar.add(fileMenu);
 
+        JMenuItem newGame = new JMenuItem("New Game");
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+        	startGame();
+            }
+        });
+        fileMenu.add(newGame);
+        
+        //Settings pop-up menu containing text fields, so the user can enter the new connection requirements or grid size
+        JMenuItem settings = new JMenuItem("Settings");
+        settings.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+        	settingsWindow.setVisible(true);
+            }
+        });
+        fileMenu.add(settings);
+
+
+        settingsWindow = new JFrame();
+        settingsWindow.setLayout(new GridLayout(10, 10)); //Fixed size?
+        JTextField reqConnections = new JTextField("Required Connections: ");
+        reqConnections.setEditable(false);
+        settingsWindow.add(reqConnections);
+        JTextField connectionsInput = new JTextField("" + requiredConections);
+        settingsWindow.add(connectionsInput);
+        JTextField numRows = new JTextField("Current # of Rows: ");
+        numRows.setEditable(false);
+        settingsWindow.add(numRows);
+        JTextField rowsInput = new JTextField("" + gameRows);
+        settingsWindow.add(rowsInput);
+        JTextField numCol = new JTextField("Current # of Columns: ");
+        numCol.setEditable(false);
+        settingsWindow.add(numCol);
+        JTextField columnInput = new JTextField("" + gameColumns);
+        settingsWindow.add(columnInput);
+        
+        JButton saveBtn = new JButton("Save");
+        saveBtn.addActionListener(new ActionListener () { 
+            @Override 
+            public void actionPerformed(ActionEvent event) {
+        	try {
+            	int newRows = Integer.parseInt(rowsInput.getText());
+            	int newColumns = Integer.parseInt(columnInput.getText());
+            	int newConnections = Integer.parseInt(connectionsInput.getText()); 
+
+            	//TODO: Need to include code to validate that numConnections is not bigger than the number of rows or columns
+            	
+        	if (newRows > ROW_MAX || newRows < ROW_MIN)
+        	{
+        	    JOptionPane.showMessageDialog(settingsWindow, "Rows must be between " + ROW_MIN + " and " + ROW_MAX);
+        	}
+        	else if (newColumns > COLUMN_MAX || newColumns < COLUMN_MIN)
+        	{
+        	    JOptionPane.showMessageDialog(settingsWindow, "Columns must be between " + COLUMN_MIN + " and " + COLUMN_MAX);
+        	}
+        	else if (newConnections > CONNECTIONS_MAX || newConnections < CONNECTIONS_MIN)
+        	{
+        	    JOptionPane.showMessageDialog(settingsWindow, "Connections must be between " + CONNECTIONS_MIN + " and " + CONNECTIONS_MAX);
+        	}
+        	else
+        	{
+
+            	settingsWindow.setVisible(false);
+            	gameRows = newRows;
+            	gameColumns = newColumns;
+            	requiredConections = newConnections;
+            	
+            	gameGrid = new double[gameRows][gameColumns];
+            	uiPanel.repaint();   
+        	}
+        	}
+        	catch (NumberFormatException e){
+        	    JOptionPane.showMessageDialog(settingsWindow, "Please enter only integers");
+        	}
+        	
+        	}
+        });
+        
+        //Exits the window and resets the text fields to previous values if user hits cancel
+        //TODO: If user exits the window by hitting "X" or pressing alt+f4(Windows) or command-w(Mac), then the text fields do not get reset. Need to fix this somehow.
+        JButton cancelBtn = new JButton("Cancel");
+        cancelBtn.addActionListener(new ActionListener () { 
+            @Override 
+            public void actionPerformed(ActionEvent event) { 
+        	settingsWindow.setVisible(false);
+        	rowsInput.setText(String.valueOf(gameRows));
+        	columnInput.setText(String.valueOf(gameColumns));
+        	connectionsInput.setText(String.valueOf(requiredConections));
+            }});
+        
+        settingsWindow.add(saveBtn);
+        settingsWindow.add(cancelBtn);
+        
+        settingsWindow.pack();
+        
+        frame.setJMenuBar(menuBar);
         frame.add(uiPanel);
         frame.pack();
         frame.setVisible(true);
-
+    }
+    
+    public void startGame() {
+    	win = false;
+    	
+    	for (int row = 0; row < gameRows; row++)
+    	{
+    	    for (int col = 0; col < gameColumns; col++)
+    	    {
+    		gameGrid[row][col] = 0.0;
+    	    }
+    	}
+    	uiPanel.repaint();
     }
 }
